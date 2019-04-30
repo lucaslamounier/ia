623d88952c956329e4010a33114c8fed291ba001
Lucas Lamounier <lucaslamounier@users.noreply.github.com>
2019-04-30 17:19:10 -0300

    atualizando exemplo

--- deteccao-de-faces-com-python-e-opencv/codigo-font/Exemplo1.py
+++ deteccao-de-faces-com-python-e-opencv/codigo-font/Exemplo1.py
@@ -2,17 +2,21 @@ import cv2
 
 classificador = cv2.CascadeClassifier('../cascades/haarcascade_frontalface_default.xml')
 
-imagem = cv2.imread('../pessoas/beatles.jpg')
+imagem = cv2.imread('../pessoas/pessoas3.jpg')
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-faces_detectadas = classificador.detectMultiScale(imagemCinza)
+# faces_detectadas = classificador.detectMultiScale(imagemCinza)
+
+# Adicionado parametro de escala para melhorar a precisão
+faces_detectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.1)
+
 print(len(faces_detectadas))
 print(faces_detectadas)
 
 for (x, y, l, a) in faces_detectadas:
     print(x, y, l, a)
     # Parametros Imagem, Posição original, Quanto quer desenhar da borda, Valores RGB da borda, Espessura da borda
-    cv2.rectangle(imagem, (x,y), (x + l, y + a), (0, 0, 255), 2)
+    cv2.rectangle(imagem, (x,y), (x + l, y + a), (0, 253, 255), 2)
 
 cv2.imshow("Faces encontradas", imagem)
 cv2.waitKey()
